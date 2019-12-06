de0fa74d2d980d1b7d3b95e883d88d054d89d3d9
Alexander <alexzaets.dev@gmail.com>
2019-12-06 16:23:44 +0200

    1. fix the semaphore way

--- SemaphoreMutex.php
+++ SemaphoreMutex.php
@@ -47,10 +47,7 @@ class SemaphoreMutex extends \yii\mutex\Mutex
                     return true;
                 }
                 if (!is_null($timeout) && ($res = time()-$start) >= $timeout) {
-                    if (file_exists("/tmp/$name")) {
-                        unlink("/tmp/$name");
-                    }
-                    Yii::info("Lock wasnt received after $timeout seconds. Give up.", 'dev');
+                    Yii::trace("Lock wasnt received after $timeout seconds. Give up.", 'dev');
                     Yii::endProfile("Waiting for lock of $origName", 'AtomicLock::receive');
                     return false;
                 }
