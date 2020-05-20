6da357afdff978de484167b0aefab689d159a724
Alexander Chertovsky <alexzaets.dev@gmail.com>
2020-05-20 14:00:22 +0300

    1. action required to make autoRelease work

--- SemaphoreMutex.php
+++ SemaphoreMutex.php
@@ -13,7 +13,11 @@ class SemaphoreMutex extends \yii\mutex\Mutex
     {
         return parent::acquire($name, $timeout);
     }
+
     /**
+     * sem_release and sem_remove requires sem_id.
+     * This is the place to store it
+     *
      * @var array $semaphores
      */
     protected $semaphores = [];
@@ -29,8 +33,6 @@ class SemaphoreMutex extends \yii\mutex\Mutex
     public function acquireLock($name, $timeout = null, $limit = 1)
     {
         //fix issue with reserved characters by hashing the name
-        $origName = $name;
-        $name = md5($name);
         try {
             if (!file_exists("/tmp/$name")) {
                 touch("/tmp/$name");
@@ -38,18 +40,18 @@ class SemaphoreMutex extends \yii\mutex\Mutex
             $semKey = ftok("/tmp/$name", 'a');
             $semId = sem_get($semKey, $limit);
             $start = time();
-            Yii::beginProfile("Waiting for lock of $origName", 'AtomicLock::receive');
+            Yii::beginProfile("Waiting for lock of $name", 'AtomicLock::receive');
             while (1) {
                 if (sem_acquire($semId, is_null($timeout) ? false : true)) {
-                    Yii::trace("Aquire: success: Name: $origName", 'semaphore');
-                    Yii::endProfile("Waiting for lock of $origName", 'AtomicLock::receive');
-                    Yii::beginProfile("Total time in $origName lock", 'AtomicLock::receive');
+                    Yii::trace("Aquire: success: Name: $name", 'semaphore');
+                    Yii::endProfile("Waiting for lock of $name", 'AtomicLock::receive');
+                    Yii::beginProfile("Total time in $name lock", 'AtomicLock::receive');
                     $this->semaphores[$name] = $semId;
                     return true;
                 }
                 if (!is_null($timeout) && (time()-$start) >= $timeout) {
-                    Yii::trace("Aquire: Failure. Name: $origName. Reason: Lock wasnt received after $timeout seconds. Give up.", 'semaphore');
-                    Yii::endProfile("Waiting for lock of $origName", 'AtomicLock::receive');
+                    Yii::trace("Aquire: Failure. Name: $name. Reason: Lock wasnt received after $timeout seconds. Give up.", 'semaphore');
+                    Yii::endProfile("Waiting for lock of $name", 'AtomicLock::receive');
                     return false;
                 }
                 sleep(1);
@@ -71,23 +73,17 @@ class SemaphoreMutex extends \yii\mutex\Mutex
      */
     public function releaseLock($name)
     {
-        $origName = $name;
-        $name = md5($name);
-        if (!isset($this->semaphores[$name])) {
-            Yii::trace("Release: failure. Name: $origName. No semaphore was found in array ".var_export($this->semaphores, true), 'semaphore');
-            return true;
-        }
         $semId = $this->semaphores[$name];
         $result = sem_release($semId);
         if ($result) {
-            Yii::trace("Release: success. Name: $origName", 'semaphore');
+            Yii::trace("Release: success. Name: $name", 'semaphore');
         } else {
-            Yii::error("Release: failure. Name: $origName", 'semaphore');
+            Yii::error("Release: failure. Name: $name", 'semaphore');
         }
         if (file_exists("/tmp/$name")) {
             unlink("/tmp/$name");
         }
-        Yii::endProfile("Total time in $origName lock", 'AtomicLock::receive');
+        Yii::endProfile("Total time in $name lock", 'AtomicLock::receive');
         return true;
     }
 
