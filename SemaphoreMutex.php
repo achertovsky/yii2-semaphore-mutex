b75d57b1da03c24676de82ae43ab96d6d27fa215
Alexander Chertovsky <alexzaets.dev@gmail.com>
2020-05-20 13:23:23 +0300

    1. same category for semaphore logs

--- SemaphoreMutex.php
+++ SemaphoreMutex.php
@@ -41,21 +41,21 @@ class SemaphoreMutex extends \yii\mutex\Mutex
             Yii::beginProfile("Waiting for lock of $origName", 'AtomicLock::receive');
             while (1) {
                 if (sem_acquire($semId, is_null($timeout) ? false : true)) {
-                    Yii::trace("Lock was received");
+                    Yii::trace("Lock was received", 'semaphore');
                     Yii::endProfile("Waiting for lock of $origName", 'AtomicLock::receive');
                     Yii::beginProfile("Total time in $origName lock", 'AtomicLock::receive');
                     $this->semaphores[$name] = $semId;
                     return true;
                 }
                 if (!is_null($timeout) && ($res = time()-$start) >= $timeout) {
-                    Yii::trace("Lock wasnt received after $timeout seconds. Give up.", 'dev');
+                    Yii::trace("Lock wasnt received after $timeout seconds. Give up.", 'semaphore');
                     Yii::endProfile("Waiting for lock of $origName", 'AtomicLock::receive');
                     return false;
                 }
                 sleep(1);
             }
         } catch (\Exception $ex) {
-            Yii::error($ex->getMessage().' '.$ex->getTraceAsString());
+            Yii::error($ex->getMessage().' '.$ex->getTraceAsString(), 'semaphore');
             if (file_exists("/tmp/$name")) {
                 unlink("/tmp/$name");
             }
@@ -74,7 +74,7 @@ class SemaphoreMutex extends \yii\mutex\Mutex
         $origName = $name;
         $name = md5($name);
         if (!isset($this->semaphores[$name])) {
-            Yii::info("No semaphore was released by name $name ".var_export($this->semaphores, true));
+            Yii::info("No semaphore was released by name $name ".var_export($this->semaphores, true), 'semaphore');
             return true;
         }
         $semId = $this->semaphores[$name];
